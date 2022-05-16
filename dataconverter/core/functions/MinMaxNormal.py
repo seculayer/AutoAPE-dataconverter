2855d1420794709c5651a2418b4ee86e94e694f0
이원준 <wonjoon.lee@seculayer.com>
2022-05-16 13:20:41 +0900

    type hint, test 추가: MinMaxNormal

--- dataconverter/core/functions/MinMaxNormal.py
+++ dataconverter/core/functions/MinMaxNormal.py
@@ -3,12 +3,20 @@
 # e-mail : jinkim@seculayer.co.kr
 # Powered by Seculayer © 2021 Service Model Team, Intelligence R&D Center.
 
+from __future__ import annotations
+
+from typing import Optional, Union
+
 import numpy as np
 
 from dataconverter.core.ConvertAbstract import ConvertAbstract
 
 
 class MinMaxNormal(ConvertAbstract):
+    # TODO: apply, reserve의 타입이 다른 것 같다. 이 둘을 통일시켜야 할 필요가 있어보인다
+    max: float
+    min: float
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         try:
@@ -20,34 +28,31 @@ class MinMaxNormal(ConvertAbstract):
             self.max = 0
             self.min = 0
 
-    def apply(self, data):
+    def apply(self, data: Union[float, str]) -> list[Optional[float]]:
         norm = self.max - self.min
-        normalized = None
         # zero-division protection
         if norm == 0:
             # self.LOGGER.warn("Min val is same Max val (Min val == Max val)")
             norm = 1
 
         try:
-            temp_result = np.array(data, dtype=np.float)
             # temp_result = float(data) / 255
             # temp_result = (float(data) - self.min) / (self.max - self.min)
-            temp_result: np.array = (temp_result - self.min) / norm
-            normalized = temp_result.tolist()
+            result: np.ndarray = (np.array(data, dtype=np.float32) - self.min) / norm
+            return [result.tolist()]
         except Exception as e:
             if not self.error_log_flag:
                 # print log for error
-                self.LOGGER.error("[MinMaxNormal] Convert error !!! self.min : {}, self.max : {}, data : {}".format(self.min, self.max, data))
+                self.LOGGER.error(
+                    "[MinMaxNormal] Convert error !!! self.min : {}, self.max : {}, data : {}".format(
+                        self.min, self.max, data
+                    )
+                )
                 self.LOGGER.error(e, exc_info=True)
                 self.error_log_flag = True
+            return [None]
 
-        # List return
-        result = list()
-        result.append(normalized)
-
-        return result
-
-    def reverse(self, data):
+    def reverse(self, data: float) -> Optional[float]:
         result = None
         norm = self.max - self.min
         # zero-division protection
@@ -56,23 +61,24 @@ class MinMaxNormal(ConvertAbstract):
             norm = 1
 
         try:
-            temp_result = np.array(data, dtype=np.float)
-            temp_result: np.array = temp_result * norm + self.min
+            temp_result: np.ndarray = np.array(data, dtype=np.float32) * norm + self.min
             result = temp_result.tolist()
         except Exception as e:
             if not self.error_log_flag:
                 # print log for error
-                self.LOGGER.error("[MinMaxNormal] Convert error !!! self.min : {}, self.max : {}, data : {}".format(self.min, self.max, data))
+                self.LOGGER.error(
+                    "[MinMaxNormal] Convert error !!! self.min : {}, self.max : {}, data : {}".format(
+                        self.min, self.max, data
+                    )
+                )
                 self.LOGGER.error(e, exc_info=True)
                 self.error_log_flag = True
 
         return result
 
 
-if __name__ == '__main__':
-    minmax_normalization = MinMaxNormal(
-        stat_dict={"min": 0, "max": 255}, arg_list=None
-    )
+if __name__ == "__main__":
+    minmax_normalization = MinMaxNormal(stat_dict={"min": 0, "max": 255}, arg_list=None)
     ip = "192.168.2.236"
     ip_split = ip.split(".")
 
