002c61ead17cd73593a3bd6c5308a7ac7e1aafaa
이원준 <wonjoon.lee@seculayer.com>
2022-05-17 09:59:35 +0900

    type hint, test 추가: PortNormal

--- dataconverter/core/functions/PortNormal.py
+++ dataconverter/core/functions/PortNormal.py
@@ -3,33 +3,36 @@
 # e-mail : bmg8551@seculayer.co.kr
 # Powered by Seculayer © 2021 Service Model Team
 
+from __future__ import annotations
+
+from typing import Optional, SupportsFloat, Union
+
 from dataconverter.core.ConvertAbstract import ConvertAbstract
 
 
 class PortNormal(ConvertAbstract):
+    _max: int = 65535
+    _min: int = 0
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.max = 65535
-        self.min = 0
 
-    def apply(self, data):
-        norm = self.max - self.min
-        temp_result = None
+    def apply(self, data: Union[SupportsFloat, str, float]) -> list[Optional[float]]:
+        norm = self._max - self._min
+        result = None
         try:
             # Normalization
-            temp_result = (float(data) - self.min) / norm
+            result = (float(data) - self._min) / norm
             # temp_result = float(data) / 65535
         except Exception as e:
             # print log for error
             self.LOGGER.error(str(e))
 
         # List return
-        result = list()
-        result.append(temp_result)
-        return result
+        return [result]
 
 
 if __name__ == "__main__":
-    port_num = '8080'
+    port_num = "8080"
     port_normal = PortNormal()
-    print(port_normal.apply(data = port_num))
+    print(port_normal.apply(data=port_num))
