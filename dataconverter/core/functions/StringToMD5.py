50a8cc4150ad27608ea6d26d5c36b8974ba592de
이원준 <wonjoon.lee@seculayer.com>
2022-05-19 16:53:43 +0900

    type hint, test 추가: StringToMD5

--- dataconverter/core/functions/StringToMD5.py
+++ dataconverter/core/functions/StringToMD5.py
@@ -3,7 +3,10 @@
 # e-mail : syjo@seculayer.co.kr
 # Powered by Seculayer © 2018 AI-Core Team
 
+from __future__ import annotations
+
 import hashlib
+from typing import Union
 
 from dataconverter.core.ConvertAbstract import ConvertAbstract
 
@@ -12,27 +15,27 @@ class StringToMD5(ConvertAbstract):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def apply(self, data):
-        result = ''
+    def apply(self, data: str) -> list[str]:
+        result = ""
 
         # check blank
-        if self._isBlank(data) :
+        if self._isBlank(data):
             return [result]
 
         result = self._strToMD5hash(data)
 
         return [result]
 
-    def _strToMD5hash(self, _str):
+    def _strToMD5hash(self, string: str) -> str:
         try:
             m = hashlib.md5()
-            m.update(_str.encode('UTF-8'))
+            m.update(string.encode("UTF-8"))
             return m.hexdigest()
         except Exception as e:
             self.LOGGER.error(e)
-            return _str
+            return string
 
 
 if __name__ == "__main__":
-    str = "Korea"
-    print(StringToMD5().apply(str))
+    string = "Korea"
+    print(StringToMD5().apply(string))
