58d993a67745de8ef3f9227537352ab6249f1767
문일주 대리 <ilju.mun@seculayer.com>
2023-07-17 10:08:08 +0900

    - DataConverter 모듈 ImageNormalization Class 추가

---
+++ dataconverter/core/functions/ImageNormalization.py
@@ -0,0 +1,31 @@
+from typing import Any, List
+from pycmmn.utils.CV2Utils import CV2Utils
+from dataconverter.core.ConvertAbstract import ConvertAbstract
+from dataconverter.common.Constants import Constants
+
+class ImageNormalization(ConvertAbstract):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.num_feat = 1
+        self.return_type = Constants.RETURN_TYPE_FLOAT
+
+    def apply(self, data:Any) -> list:
+        _cv2 = CV2Utils.get_cv2()
+        img = _cv2.cvtColor(data, _cv2.COLOR_BGR2GRAY)
+        img_norm = _cv2.normalize(img, None, 0, 255, _cv2.NORM_MINMAX)
+
+        return [img_norm]
+
+
+
+if __name__ == '__main__':
+    from pycmmn.utils.ImageUtils import ImageUtils
+    image_path = "/home/autoAPE"
+    __cv2 = CV2Utils.get_cv2()
+    with open(f"{image_path}/iris.jpg", "rb") as img_f:
+        img_arr = img_f.read()
+        img_data = ImageUtils.load(img_arr)
+
+    rst = ImageNormalization(stat_dict=None, arg_list=None).apply(img_data)
+    print(rst[0])
+    __cv2.imwrite(f"{image_path}/normalization_rst.jpg", rst[0])
