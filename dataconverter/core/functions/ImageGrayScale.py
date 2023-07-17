c2a167e8c7f7174f1bac816e5fc2bb860a5e1d73
문일주 대리 <ilju.mun@seculayer.com>
2023-07-17 10:09:22 +0900

    - DataConverter 모듈 ImageGrayScale Class 추가

---
+++ dataconverter/core/functions/ImageGrayScale.py
@@ -0,0 +1,30 @@
+from typing import Any, List
+from pycmmn.utils.CV2Utils import CV2Utils
+from dataconverter.core.ConvertAbstract import ConvertAbstract
+from dataconverter.common.Constants import Constants
+
+class ImageGrayScale(ConvertAbstract):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.num_feat = 1
+        self.return_type = Constants.RETURN_TYPE_FLOAT
+
+    def apply(self, data: Any) -> list:
+        _cv2 = CV2Utils.get_cv2()
+        gray_scale = _cv2.cvtColor(data, _cv2.COLOR_BGR2GRAY)
+
+        return [gray_scale]
+
+
+if __name__ == '__main__':
+    from pycmmn.utils.ImageUtils import ImageUtils
+    image_path = "/home/autoAPE"
+    __cv2 = CV2Utils.get_cv2()
+    with open(f"{image_path}/iris.jpg", "rb") as img_f:
+        img_arr = img_f.read()
+        img_data = ImageUtils.load(img_arr)
+
+    rst = ImageGrayScale(stat_dict=None, arg_list=None).apply(img_data)
+    print(rst[0])
+    __cv2.imwrite(f"{image_path}/gray_rst.jpg", rst[0])
+
